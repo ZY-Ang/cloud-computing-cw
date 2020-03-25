8b518dcb1e45d56ab48e538408613eedf8d4755a
Alex Ang <angzhanyu@gmail.com>
2020-03-25 16:42:12 +0800

    Update wp-config.php
    New plugin settings

--- awswordpress/lambda/src/php/wp-config.php
+++ awswordpress/lambda/src/php/wp-config.php
@@ -41,10 +41,45 @@ define('WP_SITEURL', 'https://cloudfront_domain_here');
 define('WP_HOME', 'https://cloudfront_domain_here');
 $_SERVER['HTTP_HOST'] = 'cloudfront_domain_here';
 
-define( 'S3_UPLOADS_BUCKET', getenv('S3_UPLOADS_BUCKET') . '/wp-content' );
-define( 'S3_UPLOADS_USE_INSTANCE_PROFILE', true );
-define( 'S3_UPLOADS_DISABLE_REPLACE_UPLOAD_URL', true );
-define( 'S3_UPLOADS_REGION', getenv('AWS_REGION') );
+define( 'AS3CF_SETTINGS', serialize( array(
+    // Storage Provider ('aws', 'do', 'gcp')
+    'provider' => 'aws',
+    // Access Key ID for Storage Provider (aws and do only, replace '*')
+//    'access-key-id' => '********************',
+    // Secret Access Key for Storage Providers (aws and do only, replace '*')
+//    'secret-access-key' => '**************************************',
+    // GCP Key File Path (gcp only)
+    // Make sure hidden from public website, i.e. outside site's document root.
+//    'key-file-path' => '/path/to/key/file.json',
+    // Use IAM Roles on Amazon Elastic Compute Cloud (EC2) or Google Compute Engine (GCE)
+    'use-server-roles' => true,
+    // Bucket to upload files to
+    'bucket' => getenv('S3_UPLOADS_BUCKET'),
+    // Bucket region (e.g. 'us-west-1' - leave blank for default region)
+    'region' => getenv('AWS_REGION'),
+    // Automatically copy files to bucket on upload
+    'copy-to-s3' => true,
+    // Rewrite file URLs to bucket
+    'serve-from-s3' => true,
+    // Bucket URL format to use ('path', 'cloudfront')
+    'domain' => 'cloudfront',
+    // Custom domain if 'domain' set to 'cloudfront'
+    'cloudfront' => 'cloudfront_domain_here',
+    // Enable object prefix, useful if you use your bucket for other files
+    'enable-object-prefix' => true,
+    // Object prefix to use if 'enable-object-prefix' is 'true'
+    'object-prefix' => 'wp-content/uploads/',
+    // Organize bucket files into YYYY/MM directories
+    'use-yearmonth-folders' => true,
+    // Serve files over HTTPS
+    'force-https' => false,
+    // Remove the local file version once offloaded to bucket
+    'remove-local-file' => true,
+    // Append a timestamped folder to path of files offloaded to bucket
+    'object-versioning' => false,
+) ) );
+
+define('FS_METHOD', 'direct' );
 
 // Disable online file modification. Lambda runs in a read-only file system, so
 // this just hides options that would result in errors anyway.
