2628e580c15b18bca0882128126f39ddcd8097f1
Alex Ang <angzhanyu@gmail.com>
2020-03-25 16:03:28 +0800

    Update handler.php
    Remove unnecessary code

--- awswordpress/lambda/src/php/handler.php
+++ awswordpress/lambda/src/php/handler.php
@@ -1,7 +1,5 @@
 <?php
 
-// A special handler.
-
 // All requests through API Gateway are HTTPS.
 $_SERVER['HTTPS'] = 'on';
 
@@ -23,32 +21,21 @@ if ( $local_file_path == __FILE__ ) {
     exit();
 }
 
-// echo '<pre>';
-// var_dump($_SERVER);
-// echo '</pre>';
-
-// echo "X-ScriptFilename: " . $local_file_path;
-// exit();
-
 $split = explode(".", $local_file_path);
 $extension = end($split);
 $mapped_type = $extension_map[$extension];
 
 if ( $mapped_type && file_exists( $local_file_path ) ) {
-    header("Content-Type: {$mapped_type}");
     readfile($local_file_path);
 
 } elseif ( $extension == "php" && file_exists( $local_file_path ) ) {
-    header("X-ExecFile: {$local_file_path}");
     require( $local_file_path );
 
 } elseif ( substr($local_file_path, -1) == "/" && file_exists( $local_file_path . "index.php" ) ) {
     $exec_file_path = $local_file_path . "index.php";
-    header("X-ExecFile: {$exec_file_path}");
     require( $exec_file_path );
 
 } else {
     $exec_file_path = dirname(__FILE__) . '/index.php';
-    header("X-ExecFile: {$exec_file_path}");
     require( $exec_file_path );
 }
